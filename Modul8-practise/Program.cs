2264c0320e34165e3a860b2d4365f34c69fc5d87
Nurik <tanatov1989@mail.ru>
2017-12-25 16:09:09 +0600

    Добавьте файлы проекта.

---
+++ Modul8-practise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace practise_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int low_index;
+            int upper_index;
+            WriteLine("Введите наименьший индекс");
+            while (!int.TryParse(ReadLine(), out low_index))
+            {
+                WriteLine("Error");
+            }
+            WriteLine("Введите наибольший индекс");
+            while (!int.TryParse(ReadLine(), out upper_index))
+            {
+                WriteLine("Error");
+            }
+            Clear();
+            Random rand = new Random();
+
+            if (!(low_index > upper_index))
+            {
+                RangeOfArray array = new RangeOfArray(low_index, upper_index);
+                for (int i = 0; i < array.arr.Length; i++)
+                {
+                    array.arr[i] = rand.Next(10);
+                }
+                array.PrintArray(array, low_index);
+            }
+
+            ReadLine();
+
+        }
+    }
+
+}
+
---
+++ Modul8-practise/RangeOfArray.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace practise_work
+{
+    public class RangeOfArray
+    {
+        public int lowest_index;
+        public int uppest_index;
+        public int[] arr;
+        public RangeOfArray(int low_index, int upper_index)
+        {
+            this.lowest_index = low_index;
+            this.uppest_index = upper_index;
+            arr = new int[(upper_index - low_index) + 1];
+        }
+        public void PrintArray(RangeOfArray array, int lower_index)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                WriteLine((i + lower_index) + " элемент массива " + arr[i]);
+            }
+        }
+
+
+
+
+    }
+}
